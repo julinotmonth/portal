138b85b8c4c4aa7f92be6a20f69470911859c27a
jeje <akhmdjzl17@gmail.com>
2024-11-11 10:27:24 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function App() {
   return (
     <BrowserRouter>
           <Routes>
-              <Route path='/' element={<Beranda />} />
+              <Route path='/' element={<LoginPages />} />
               <Route path='/dashboard' element={<PagesDashboard />} />
               <Route path='/dashboard/berita' element={<PagesDashBerita />} />
               <Route path='/dashboard/berita/add-berita' element={<AddProDash />} />
